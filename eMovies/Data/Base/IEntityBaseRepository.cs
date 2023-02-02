d5add14578abff5efdf60d56a1845a37db5f6f04
Daafiina <da52811@ubt-uni.net>
2023-02-02 22:58:05 +0100

    updated base

--- eMovies/Data/Base/IEntityBaseRepository.cs
+++ eMovies/Data/Base/IEntityBaseRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace eMovies.Data.Base
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eMovies.Data.Base
 {
-    public interface IEntityBaseRepository
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
     }
 }
