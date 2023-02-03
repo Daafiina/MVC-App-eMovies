4d000f5f7ae38603e4cc514428a583ce5646aea4
mirjeta rexhepi <mr51033@ubt-uni.net>
2023-02-03 15:47:40 +0100

    Cinema crud

---
+++ eMovies/Data/Services/CinemasService.cs
@@ -0,0 +1,18 @@
+﻿using eMovies.Data.Base;
+using eMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMovies.Data.Services
+{
+    public class CinemasService:EntityBaseRepository<Cinema>, ICinemasService
+    {
+
+        public CinemasService(AppDbContext context):base(context)
+        {
+
+        }
+    }
+}
--- eMovies/Startup.cs
+++ eMovies/Startup.cs
@@ -33,6 +33,7 @@ namespace eMovies
             services.AddScoped<IActorsService,ActorsService>();
 
             services.AddControllersWithViews();
+            services.AddScoped<ICinemasService, CinemasService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
