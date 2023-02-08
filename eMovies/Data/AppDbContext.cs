749089793240816362ec894f5ad7ec491bfac893
Daafiina <da52811@ubt-uni.net>
2023-02-08 02:07:29 +0100

    added identity tables

--- eMovies/Data/AppDbContext.cs
+++ eMovies/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using eMovies.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace eMovies.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -34,5 +35,6 @@ namespace eMovies.Data
         public DbSet<OrderItem> OrderItems { get; set; }
 
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+
     }
 }
---
+++ eMovies/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eMovies.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Display(Name ="Fullname")]
+        public  string FullName { get; set; }
+
+    }
+}
